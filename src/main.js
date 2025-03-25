d13632a716d2bfbdf3b25cfcaf16b566941fe6ec
Shii108 <sujalpoudel01@gmail.com>
2025-03-25 10:57:22 +0545

    added spacing and comments

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
+// Vue and plugins
 import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import router from "./components/router/index.js";
 import { createPinia } from "pinia";
 
+// Main App component
+import App from "./App.vue";
+
+// Router
+import router from "./components/router/index.js";
+
+// Styles
+import "./style.css";
+
+// Create Vue application
 const app = createApp(App);
+
+// Register plugins
 app.use(router);
 app.use(createPinia());
+
+// Mount the application
 app.mount("#app");
-
-
