665b2daa6a64ce4dfa1b50441dcb6a15336eab1c
Shii108 <sujalpoudel01@gmail.com>
2025-03-31 12:34:05 +0545

    converted my normal css into scss in this cart-container

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -13,7 +13,7 @@
             </div>
           </div>
         </div>
-        <div class="cart-div" @click="showCart = true">
+        <div class="cart-icon-div" @click="showCart = true">
           <svg width="32" height="32" viewBox="0 0 24 24" fill="none">
             <path
               d="M5 14h13.14c1.01 0 1.52 0 1.92-.19.36-.17.66-.44.87-.78.23-.38.29-.88.41-1.89l.59-5.26c.03-.3.05-.45-.04-.61-.08-.1-.15-.18-.26-.23C21.57 5 21.42 5 21.1 5H4.5M2 2h1.25c.26 0 .4 0 .5.05.1.05.17.12.22.2.06.1.07.23.08.5l.91 14.5c.02.26.03.39.09.49.05.09.12.16.21.2.1.05.24.05.49.05H19M7.5 21.5h.01M16.5 21.5h.01M8 21.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Zm9 0a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Z"
@@ -22,38 +22,36 @@
           <div>{{ cartStore.totalItem }}</div>
         </div>
       </nav>
-      <div class="cart-layout">
-        <div v-if="showCart">
-          <div class="cart-content">
-            <div class="cart-upper">
-              <p>Shopping Cart Preview</p>
-              <div class="close-btn" @click="showCart = false"></div>
-            </div>
-            <div class="item-containter">
-              <div v-for="item in cartStore.item_details" :key="item.id" class="cart-item">
-                <div class="img-checkbox">
-                  <input type="checkbox" v-model="item.checked" @click="cartStore.updateChecked(item.id)" />
-                  <img :src="item.image" alt="Product Image" class="cart-img" />
-                </div>
-                <div class="detailed-info">
-                  <div class="cart-title">{{ item.title }}</div>
-                  <p class="price">Price: ${{ item.price }}</p>
-                  <div class="quantity-delete">
-                    <div class="quantity">
-                      <button @click="updatePositive(item, item.quantity, 'sub')" :disabled="item.quantity === 1"
-                        class="decrease-btn">-</button>
-                      <p>{{ item.quantity }}</p>
-                      <button @click="updatePositive(item, item.quantity, 'add')">+</button>
-                    </div>
-                    <img src="/assets/delete.svg" alt="deleteicon" @click="deleteItem(item)" />
+      <div v-if="showCart">
+        <div class="cart-content">
+          <div class="cart-upper">
+            <p>Shopping Cart Preview</p>
+            <div class="close-btn" @click="showCart = false"></div>
+          </div>
+          <div class="item-containter">
+            <div v-for="item in cartStore.item_details" :key="item.id" class="cart-item">
+              <div class="img-checkbox">
+                <input type="checkbox" v-model="item.checked" @click="cartStore.updateChecked(item.id)" />
+                <img :src="item.image" alt="Product Image" class="cart-img" />
+              </div>
+              <div class="detailed-info">
+                <div class="cart-title">{{ item.title }}</div>
+                <p class="price">Price: ${{ item.price }}</p>
+                <div class="quantity-delete">
+                  <div class="quantity">
+                    <button @click="updatePositive(item, item.quantity, 'sub')"
+                      :disabled="item.quantity === 1">-</button>
+                    <p>{{ item.quantity }}</p>
+                    <button @click="updatePositive(item, item.quantity, 'add')">+</button>
                   </div>
+                  <img src="/assets/delete.svg" alt="deleteicon" @click="deleteItem(item)" />
                 </div>
               </div>
             </div>
-            <div class="cart-lower">
-              <button class="shopping-cart" @click="goToCart()">Shopping Cart</button>
-              <button class="checkout" @click="goToCheckout()">Checkout</button>
-            </div>
+          </div>
+          <div class="cart-lower">
+            <button class="shopping-cart" @click="goToCart()">Shopping Cart</button>
+            <button class="checkout" @click="goToCheckout()">Checkout</button>
           </div>
         </div>
       </div>
@@ -66,7 +64,7 @@
 import { ref, onMounted, provide } from "vue";
 import { useRouter } from "vue-router";
 import { useCartStore } from "./store/cartStore";
- 
+
 // Router & Store
 const router = useRouter();
 const cartStore = useCartStore();
@@ -180,13 +178,23 @@ const updatePositive = (item, quantity, action) => {
 onMounted(fetchProducts);
 </script>
 
-<style scoped>
-  .page-scroll {
-    height: 100dvh;
-    width: 100%;
-    overflow: hidden;
-    pointer-events: none;
-  }
+<style scoped lang="scss">
+.page-scroll {
+  height: 100dvh;
+  width: 100%;
+  overflow: hidden;
+  pointer-events: none;
+}
+
+.page-overlay {
+  background: rgba(0, 0, 0, 0.5);
+  height: 100vh;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
 .category {
   margin-top: 10px;
   padding: 0 20px;
@@ -225,13 +233,13 @@ nav {
 .list {
   padding: 8px;
   cursor: pointer;
+
+  &:hover {
+    background-color: #f0f0f0;
+  }
 }
 
-.list:hover {
-  background-color: #f0f0f0;
-}
-
-.cart-div {
+.cart-icon-div {
   margin-left: auto;
   display: flex;
   gap: 3px;
@@ -248,103 +256,102 @@ nav {
   right: 0px;
   z-index: 100;
   pointer-events: auto;
-}
 
-.cart-upper {
-  color: #121926;
-  border-bottom: solid 1px gray;
-  padding: 20px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 10dvh;
-}
+  .cart-upper {
+    color: #121926;
+    border-bottom: solid 1px gray;
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 10dvh;
 
-.cart-upper p {
-  font-size: 2rem;
-  font-weight: 600;
-  word-spacing: -0.4px;
-}
+    p {
+      font-size: 2rem;
+      font-weight: 600;
+      word-spacing: -0.4px;
+    }
+  }
 
-.item-containter {
-  height: 80dvh;
-  overflow: auto;
-}
+  .item-containter {
+    height: 80dvh;
+    overflow: auto;
 
-.cart-item {
-  display: flex;
-  align-items: center;
-  gap: 20px;
-  padding: 10px;
-}
+    .cart-item {
+      display: flex;
+      align-items: center;
+      gap: 20px;
+      padding: 10px;
 
-.cart-item img {
-  background: white;
-  width: 100px;
-  height: 100px;
-  object-fit: cover;
-}
+      .img-checkbox {
+        display: flex;
+        align-items: center;
+        gap: 10px;
 
-.img-checkbox {
-  display: flex;
-  align-items: center;
-  gap: 10px;
-}
+        img {
+          background: white;
+          width: 100px;
+          height: 100px;
+          object-fit: cover;
+        }
+      }
 
-.detailed-info {
-  flex-grow: 1;
-}
+      .detailed-info {
+        flex-grow: 1;
 
-.cart-title {
-  font-size: 1.8rem;
-  margin-bottom: 10px;
-}
+        .cart-title {
+          font-size: 1.8rem;
+          margin-bottom: 10px;
+        }
 
-.price {
-  font-size: 1.6rem;
-  color: #364152;
-  font-weight: 600;
-  margin-bottom: 10px;
-}
+        .price {
+          font-size: 1.6rem;
+          color: #364152;
+          font-weight: 600;
+          margin-bottom: 10px;
+        }
 
-.quantity-delete {
-  display: flex;
-  justify-content: space-between;
-}
+        .quantity-delete {
+          display: flex;
+          justify-content: space-between;
 
-.quantity-delete img {
-  background-color: #f0f0f0;
-  padding: 4px;
-  height: 30px;
-  width: 30px;
-  cursor: pointer;
-}
+          .quantity {
+            font-size: 1.6rem;
+            display: flex;
+            align-items: center;
+            gap: 10px;
 
-.quantity {
-  font-size: 1.6rem;
-  display: flex;
-  align-items: center;
-  gap: 10px;
-}
+            button {
+              width: 30px;
+              height: 30px;
+              background-color: #f0f0f0;
+              border: none;
+              cursor: pointer;
 
-.quantity button {
-  width: 30px;
-  height: 30px;
-  background-color: #f0f0f0;
-  border: none;
-  cursor: pointer;
-}
+              &:hover {
+                background-color: #e1e0e0;
+              }
+            }
 
-.quantity button:hover {
-  background-color: #e1e0e0;
-}
+            p {
+              width: 30px;
+              text-align: center;
+            }
+          }
 
-.quantity-delete p {
-  width: 30px;
-  text-align: center;
-}
+          img {
+            background-color: #f0f0f0;
+            padding: 4px;
+            height: 30px;
+            width: 30px;
+            cursor: pointer;
+          }
+        }
+      }
+    }
+  }
 
-.cart-lower {
+  .cart-lower {
   border-top: solid 1px grey;
   padding: 0 20px;
   position: relative;
@@ -353,45 +360,37 @@ nav {
   display: flex;
   align-items: center;
   gap: 15px;
-}
 
-.checkout {
-  background-color: #29303B;
-  color: #FFFFDF;
-  flex-grow: 4;
-  border-radius: 4px;
-  height: 60px;
-  font-size: 1.4rem;
-  font-weight: 500px;
-  word-spacing: -0.1px;
-}
+  .checkout {
+    background-color: #29303B;
+    color: #FFFFDF;
+    flex-grow: 4;
+    border-radius: 4px;
+    height: 60px;
+    font-size: 1.4rem;
+    font-weight: 500px;
+    word-spacing: -0.1px;
 
-.checkout:hover {
-  background-color: #323a47;
-}
+    &:hover {
+      background-color: #323a47;
+    }
+  }
 
-.shopping-cart {
-  height: 60px;
-  flex-grow: 1;
-  background-color: white;
-  border: solid 1px black;
-  border-radius: 4px;
-  font-size: 1.4rem;
-  font-weight: 500px;
-  word-spacing: -0.1px;
-}
+  .shopping-cart {
+    height: 60px;
+    flex-grow: 1;
+    background-color: white;
+    border: solid 1px black;
+    border-radius: 4px;
+    font-size: 1.4rem;
+    font-weight: 500px;
+    word-spacing: -0.1px;
 
-.shopping-cart:hover {
-  background-color: rgb(240, 237, 237);
+    &:hover {
+      background-color: rgb(240, 237, 237);
+    }
+  }
 }
-
-.page-overlay {
-  background: rgba(0, 0, 0, 0.5);
-  height: 100vh;
-  width: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
 }
 
 </style>
